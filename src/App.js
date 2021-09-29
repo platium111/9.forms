26dd6898ced6a576bb4fc4e5b585044d05aee4e4
Hiep Trinh <hieptq.social@gmail.com>
2021-09-29 23:06:16 +1000

    feat: using different form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+import UsingDifferentForm from "./UsingYupValidate";
 
 /**
- * - each time typing in input, it renders again
+ * - each time typing in input, it doesn't render again, only RENDER on submit because using set state data from useState
  * - `errors` changes value in real time, it has error inside it if data changes
  * @returns
  */
@@ -14,12 +16,16 @@ function App() {
     watch,
   } = useForm();
 
+  const [formData, setFormData] = useState();
+
   const onSubmit = (data) => {
     console.log("submit ", data);
+    setFormData(data);
   };
 
-  console.log("username =", watch("username"));
+  console.log("username =", watch("game"));
   console.log("error", errors);
+  console.log("formData", formData);
 
   return (
     <div className="App">
@@ -29,12 +35,15 @@ function App() {
           {...register("username", { required: true, maxLength: 10 })}
         ></input>
         {errors.usename && <p>Need to fill out username</p>}
-
         <input {...register("password", { required: true })}></input>
-        {errors.password && <p>Need to fill out password</p>}
+        {errors.password?.type === "required" && (
+          <p>Need to fill out password</p>
+        )}
 
-        <input type="submit" />
+        {<input type="submit" value="Submit in App" />}
       </form>
+
+      {<UsingDifferentForm register={register} errors={errors} />}
     </div>
   );
 }
---
+++ src/UsingYupValidate.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const UsingDifferentForm = () => {
+  // [learn] cannot use useForm in different components (has the same <form>)
+  // to make the same <form> using in different components, need to pass `register, handleSubmit` to other components
+
+  // [learn] can use different <form> with the same level, if it's nested, consider passing `register` through props
+  // data belong to specific <form>, different from each other
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log("UsingYupValidate data", data);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="game"
+          placeholder="Yup validate"
+          {...register("game", { required: true })}
+        ></input>
+        <p>{errors.firstName?.message}</p>
+
+        <input type="submit" value="Submit UsingYupValidate" />
+      </form>
+    </div>
+  );
+};
+
+export default UsingDifferentForm;
