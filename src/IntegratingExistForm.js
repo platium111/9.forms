23d8422436a3fd50ba559c17554ba1e6f63ac3fe
Hiep Trinh <hieptq.social@gmail.com>
2021-10-02 01:29:40 +1000

    feat: using Box styling

--- src/IntegratingExistForm.js
+++ src/IntegratingExistForm.js
@@ -2,9 +2,12 @@ import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 import { TestWrapperStyled } from "./styles/index.style";
 import { randomColor } from "./utils";
 
+const FORM_DESCRIPTION =
+  "Testing using custom component with forward ref, 3rd component";
 // [learn] using forwardRef for existing UI, can pass react-hook-form as onChange
 const Select = React.forwardRef(({ label, onChange, name }, ref) => (
   <div>
@@ -26,29 +29,41 @@ const IntegratingExistForm = () => {
   return (
     <TestWrapperStyled
       title="Test Integrating exist form"
+      description={FORM_DESCRIPTION}
       color={randomColor()}
     >
       <form onSubmit={handleSubmit(onSubmit)}>
         <Container>
-          <Select
-            label="Custom label"
-            name="firstName"
-            {...register("firstName")}
-          />
-
-          {/* [learn] Using Controller for 3rd UI
-           *  + passing name, onBlur, onChange, ref to <Textfield>
-           */}
-          <Controller
-            name="lastName"
-            control={control}
-            defaultValue=""
-            render={({ field }) => {
-              console.log("field props", field);
-              return <TextField {...field} />;
+          <Box
+            sx={{
+              bgcolor: "background.paper",
+              boxShadow: 3,
+              borderRadius: 1,
+              p: 2,
+              m: 1,
+              minWidth: 300,
             }}
-          />
+          >
+            <Select
+              label="Custom label"
+              name="firstName"
+              {...register("firstName")}
+            />
 
+            {/* [learn] Using Controller for 3rd UI
+             *  + passing name, onBlur, onChange, ref to <Textfield>
+             *  + it renders 4 time each submit (2 before, 2 after)
+             */}
+            <Controller
+              name="lastName"
+              control={control}
+              defaultValue=""
+              render={({ field }) => {
+                console.log("field props", field);
+                return <TextField {...field} />;
+              }}
+            />
+          </Box>
           <input type="submit" value="Submit IntegratingExistForm" />
         </Container>
       </form>
