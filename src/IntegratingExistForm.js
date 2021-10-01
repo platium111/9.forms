aaf8b1fdc154c2b76d3194f0aac7e349d3d3a4f1
Hiep Trinh <hieptq.social@gmail.com>
2021-10-01 23:34:34 +1000

    feat: add style component + random color generating

--- src/IntegratingExistForm.js
+++ src/IntegratingExistForm.js
@@ -2,6 +2,13 @@ import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
+import styled from "styled-components";
+
+const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const Wrapper = styled.div`
+  border: 2px solid ${randomColor};
+`;
 
 // [learn] using forwardRef for existing UI, can pass react-hook-form as onChange
 const Select = React.forwardRef(({ label, onChange, name }, ref) => (
@@ -22,25 +29,32 @@ const IntegratingExistForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Container>
-        <Select
-          label="Custom label"
-          name="firstName"
-          {...register("firstName")}
-        />
+    <Wrapper>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Container>
+          <Select
+            label="Custom label"
+            name="firstName"
+            {...register("firstName")}
+          />
 
-        {/* [learn] Using Controller for 3rd UI */}
-        <Controller
-          name="firstName"
-          control={control}
-          defaultValue=""
-          render={({ field }) => <TextField {...field} />}
-        />
+          {/* [learn] Using Controller for 3rd UI
+           *  + passing name, onBlur, onChange, ref to <Textfield>
+           */}
+          <Controller
+            name="lastName"
+            control={control}
+            defaultValue=""
+            render={({ field }) => {
+              console.log("field props", field);
+              return <TextField {...field} />;
+            }}
+          />
 
-        <input type="submit" value="Submit IntegratingExistForm" />
-      </Container>
-    </form>
+          <input type="submit" value="Submit IntegratingExistForm" />
+        </Container>
+      </form>
+    </Wrapper>
   );
 };
 
